473421b163b9fd500e9ad94c0530f4067b3d0943
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-05-02 16:40:55 -0300

    i litle trick in js

---
+++ script.js
@@ -0,0 +1,7 @@
+let verfiqued = false;
+let userName = 'afonso'
+let userSituation;
+
+userSituation= verfiqued ? userSituation ='verfiqued' : userSituation ="not verfiqued";
+
+console.log(`the user ${userName} is ${userSituation}`);
